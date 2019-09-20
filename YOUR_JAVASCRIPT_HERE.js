f57e5b2f53971a7fc374218b66fb579f3be12c9f
qiyu1987 <qiyu1987@gmail.com>
2019-09-20 19:02:42 +0200

    pass the tests

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,32 @@
 // Write your JS here
+const hero = {
+    name: 'Shela',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: 'sword',
+        damage: 2
+    }
+}
+const dagger = {
+    type: 'dagger',
+    damage: 2,
+}
+function rest(hero) {
+    if (hero.health === 10){
+        alert('Full Health Already!')
+    }
+    hero.health = 10
+    
+    return hero
+}
+function pickUpItem(hero,weapon) {
+    hero.inventory.push(weapon)
+}
+function equipWeapon(hero) {
+
+    if (hero.inventory.length !== 0){
+        hero.weapon = hero.inventory.pop()
+    }
+}
